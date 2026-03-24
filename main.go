32014ffea7536f8e7449be7076c4dec6d966156a
Mitesh Shah <Mr.Miteshah@gmail.com>
2026-03-24 22:55:37 +0530

    Updated Style

--- main.go
+++ main.go
@@ -6,16 +6,53 @@ package main
 
 #import <Cocoa/Cocoa.h>
 
-static NSStatusItem  *statusItem    = nil;
-static NSMenuItem    *vizMenuItem   = nil;
-static NSMenuItem    *secsMenuItem  = nil;
-static NSMenuItem    *countdownItem = nil;
-static NSMenuItem    *targetItem    = nil;
-static NSMenuItem    *pctItem       = nil;
+static NSStatusItem *statusItem = nil;
+static NSMenuItem   *mainItem   = nil;
 
 void initApp() {
     [NSApplication sharedApplication];
     [[NSApplication sharedApplication] setActivationPolicy:NSApplicationActivationPolicyAccessory];
+    [NSApp finishLaunching];
+}
+
+static NSAttributedString* sectionTitle(NSString *title) {
+    return [[NSAttributedString alloc]
+        initWithString:[title stringByAppendingString:@"\n\n"]
+            attributes:@{
+                NSFontAttributeName: [NSFont monospacedSystemFontOfSize:11 weight:NSFontWeightSemibold],
+                NSForegroundColorAttributeName: [NSColor colorWithWhite:0.6 alpha:1.0]
+            }];
+}
+
+static NSAttributedString* buildHeader() {
+    NSMutableAttributedString *s = [[NSMutableAttributedString alloc] init];
+
+    NSDate *now = [NSDate date];
+
+    NSDateFormatter *timeFmt = [[NSDateFormatter alloc] init];
+    [timeFmt setDateFormat:@"HH:mm:ss"];
+
+    NSDateFormatter *dateFmt = [[NSDateFormatter alloc] init];
+    [dateFmt setDateFormat:@"EEEE, MMM d, yyyy"];
+
+    NSString *timeStr = [timeFmt stringFromDate:now];
+    NSString *dateStr = [dateFmt stringFromDate:now];
+
+    [s appendAttributedString:[[NSAttributedString alloc]
+        initWithString:[timeStr stringByAppendingString:@"\n"]
+            attributes:@{
+                NSFontAttributeName: [NSFont monospacedDigitSystemFontOfSize:16 weight:NSFontWeightMedium],
+                NSForegroundColorAttributeName: [NSColor whiteColor]
+            }]];
+
+    [s appendAttributedString:[[NSAttributedString alloc]
+        initWithString:[dateStr stringByAppendingString:@"\n\n"]
+            attributes:@{
+                NSFontAttributeName: [NSFont systemFontOfSize:12],
+                NSForegroundColorAttributeName: [NSColor colorWithWhite:0.7 alpha:1.0]
+            }]];
+
+    return s;
 }
 
 static NSColor* hourColor(int hoursLeft) {
@@ -27,108 +64,127 @@ static NSColor* hourColor(int hoursLeft) {
 
 static NSAttributedString* buildHourViz(int currentH) {
     NSMutableAttributedString *s = [[NSMutableAttributedString alloc] init];
+    [s appendAttributedString:sectionTitle(@"TODAY'S 24 HOURS")];
+
     NSFont *font = [NSFont monospacedSystemFontOfSize:13.0 weight:NSFontWeightRegular];
+
     int hoursLeft = 23 - currentH;
     NSColor *fc = hourColor(hoursLeft);
-    NSColor *dim = [NSColor colorWithWhite:0.40 alpha:1.0];
-
-	// ✅ ADD THIS HEADER
-    NSString *header = [NSString stringWithFormat:@"Hours left today: %d\n\n", hoursLeft];
-    [s appendAttributedString:[[NSAttributedString alloc]
-        initWithString:header
-            attributes:@{
-                NSFontAttributeName: [NSFont systemFontOfSize:12.0 weight:NSFontWeightSemibold],
-                NSForegroundColorAttributeName: fc
-            }]];
+    NSColor *dim = [NSColor colorWithWhite:0.4 alpha:1.0];
 
     for (int h = 0; h < 24; h++) {
         NSString *label = [NSString stringWithFormat:@"%02d", h];
         NSColor *color = (h < currentH) ? dim : fc;
+
         NSMutableDictionary *attrs = [@{
             NSFontAttributeName: font,
             NSForegroundColorAttributeName: color
         } mutableCopy];
+
         if (h == currentH) {
             attrs[NSUnderlineStyleAttributeName] = @(NSUnderlineStyleSingle);
             attrs[NSUnderlineColorAttributeName] = fc;
         }
-        [s appendAttributedString:[[NSAttributedString alloc] initWithString:label attributes:attrs]];
+
+        [s appendAttributedString:[[NSAttributedString alloc]
+            initWithString:label attributes:attrs]];
+
         NSString *sep = (h % 8 == 7) ? @"\n" : @" ";
         [s appendAttributedString:[[NSAttributedString alloc]
-            initWithString:sep
-                attributes:@{NSFontAttributeName: font,
-                              NSForegroundColorAttributeName: [NSColor clearColor]}]];
+            initWithString:sep attributes:@{NSFontAttributeName: font}]];
     }
+
+    [s appendAttributedString:[[NSAttributedString alloc]
+        initWithString:@"\n\n" attributes:@{}]];
     return s;
 }
 
-static NSAttributedString* buildSecViz(int currentH, int currentS) {
+static NSAttributedString* buildMinuteViz(int currentM) {
     NSMutableAttributedString *s = [[NSMutableAttributedString alloc] init];
-    NSFont *font = [NSFont monospacedSystemFontOfSize:10.0 weight:NSFontWeightRegular];
-    NSColor *activeColor = hourColor(23 - currentH);
-    NSColor *passedColor = [activeColor colorWithAlphaComponent:0.35];
-    NSColor *dimC = [NSColor colorWithWhite:0.22 alpha:1.0];
+    [s appendAttributedString:sectionTitle(@"MINUTES THIS HOUR")];
 
-	// ✅ HEADER (same style as hours)
-	int secLeft = 59 - currentS;
-    NSString *header = [NSString stringWithFormat:@"Seconds left: %d\n\n", secLeft];
-    [s appendAttributedString:[[NSAttributedString alloc]
-        initWithString:header
-            attributes:@{
-                NSFontAttributeName: [NSFont systemFontOfSize:11.0 weight:NSFontWeightSemibold],
-                NSForegroundColorAttributeName: activeColor
-            }]];
+    NSFont *font = [NSFont monospacedSystemFontOfSize:10 weight:NSFontWeightRegular];
 
     for (int i = 0; i < 60; i++) {
-        NSColor *c = (i < currentS) ? passedColor : (i == currentS) ? activeColor : dimC;
+        NSColor *c = (i < currentM)
+            ? [NSColor colorWithRed:0.55 green:0.75 blue:0.35 alpha:1.0]
+            : [NSColor colorWithWhite:0.2 alpha:1.0];
+
+        [s appendAttributedString:[[NSAttributedString alloc]
+            initWithString:@"▮"
+                attributes:@{
+                    NSFontAttributeName: font,
+                    NSForegroundColorAttributeName: c
+                }]];
+    }
+
+    [s appendAttributedString:[[NSAttributedString alloc]
+        initWithString:@"\n\n" attributes:@{}]];
+    return s;
+}
+
+static NSAttributedString* buildSecViz(int currentS) {
+    NSMutableAttributedString *s = [[NSMutableAttributedString alloc] init];
+    [s appendAttributedString:sectionTitle(@"SECONDS THIS MINUTE")];
+
+    NSFont *font = [NSFont monospacedSystemFontOfSize:10 weight:NSFontWeightRegular];
+
+    for (int i = 0; i < 60; i++) {
+        NSColor *c = (i < currentS)
+            ? [NSColor colorWithRed:0.4 green:0.8 blue:0.7 alpha:1.0]
+            : [NSColor colorWithWhite:0.2 alpha:1.0];
+
         [s appendAttributedString:[[NSAttributedString alloc]
             initWithString:@"■"
-                attributes:@{NSFontAttributeName: font,
-                              NSForegroundColorAttributeName: c}]];
-        if (i == 29) {
-            [s appendAttributedString:[[NSAttributedString alloc]
-                initWithString:@"\n" attributes:@{NSFontAttributeName: font}]];
-        }
+                attributes:@{
+                    NSFontAttributeName: font,
+                    NSForegroundColorAttributeName: c
+                }]];
     }
+
+    [s appendAttributedString:[[NSAttributedString alloc]
+        initWithString:@"\n\n" attributes:@{}]];
+    return s;
+}
+
+static NSAttributedString* buildLegend() {
+    NSMutableAttributedString *s = [[NSMutableAttributedString alloc] init];
+
+    NSArray *items = @[
+        @{@"label": @"past", @"color": [NSColor lightGrayColor]},
+        @{@"label": @"12+ h", @"color": [NSColor colorWithRed:0.6 green:0.8 blue:0.4 alpha:1]},
+        @{@"label": @"6–12 h", @"color": [NSColor colorWithRed:0.9 green:0.7 blue:0.3 alpha:1]},
+        @{@"label": @"3–6 h", @"color": [NSColor colorWithRed:0.9 green:0.5 blue:0.3 alpha:1]},
+        @{@"label": @"0–3 h", @"color": [NSColor colorWithRed:0.9 green:0.3 blue:0.3 alpha:1]},
+    ];
+
+    for (NSDictionary *item in items) {
+        NSString *text = [NSString stringWithFormat:@"■ %@  ", item[@"label"]];
+        [s appendAttributedString:[[NSAttributedString alloc]
+            initWithString:text
+                attributes:@{
+                    NSFontAttributeName: [NSFont systemFontOfSize:11],
+                    NSForegroundColorAttributeName: item[@"color"]
+                }]];
+    }
+
     return s;
 }
 
 void createStatusItem(const char *initialTitle) {
     NSStatusBar *bar = [NSStatusBar systemStatusBar];
     statusItem = [bar statusItemWithLength:NSVariableStatusItemLength];
-    NSFont *mono = [NSFont monospacedSystemFontOfSize:12.0 weight:NSFontWeightRegular];
+
+    NSFont *mono = [NSFont monospacedDigitSystemFontOfSize:12 weight:NSFontWeightRegular];
     statusItem.button.attributedTitle = [[NSAttributedString alloc]
         initWithString:[NSString stringWithUTF8String:initialTitle]
-            attributes:@{NSFontAttributeName: mono}];
+        attributes:@{NSFontAttributeName: mono}];
 
     NSMenu *menu = [[NSMenu alloc] init];
-    [menu setAutoenablesItems:NO];
 
-    vizMenuItem = [[NSMenuItem alloc] initWithTitle:@"" action:nil keyEquivalent:@""];
-    [vizMenuItem setEnabled:NO];
-    [menu addItem:vizMenuItem];
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    secsMenuItem = [[NSMenuItem alloc] initWithTitle:@"" action:nil keyEquivalent:@""];
-    [secsMenuItem setEnabled:NO];
-    [menu addItem:secsMenuItem];
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    pctItem = [[NSMenuItem alloc] initWithTitle:@"" action:nil keyEquivalent:@""];
-    [pctItem setEnabled:NO];
-    [menu addItem:pctItem];
-
-    [menu addItem:[NSMenuItem separatorItem]];
-
-    countdownItem = [[NSMenuItem alloc] initWithTitle:@"" action:nil keyEquivalent:@""];
-    [countdownItem setEnabled:NO];
-    [menu addItem:countdownItem];
-
-    targetItem = [[NSMenuItem alloc] initWithTitle:@"" action:nil keyEquivalent:@""];
-    [targetItem setEnabled:NO];
-    [menu addItem:targetItem];
+    mainItem = [[NSMenuItem alloc] initWithTitle:@"" action:nil keyEquivalent:@""];
+    [mainItem setEnabled:NO];
+    [menu addItem:mainItem];
 
     [menu addItem:[NSMenuItem separatorItem]];
 
@@ -140,33 +196,44 @@ void createStatusItem(const char *initialTitle) {
     statusItem.menu = menu;
 }
 
-void updateAll(const char *barTitle, int currentH, int currentS,
-               const char *pctLabel, const char *countdownStr, const char *targetStr) {
-    NSString *bt  = barTitle     ? [NSString stringWithUTF8String:barTitle]     : @"";
-    NSString *pct = pctLabel     ? [NSString stringWithUTF8String:pctLabel]     : @"";
-    NSString *cd  = countdownStr ? [NSString stringWithUTF8String:countdownStr] : @"";
-    NSString *tl  = targetStr    ? [NSString stringWithUTF8String:targetStr]    : @"";
-    int h = currentH, s = currentS;
+void updateUI(const char *title, int h, int m, int s, const char *footer) {
+    NSString *t = [NSString stringWithUTF8String:title];
+    NSString *f = [NSString stringWithUTF8String:footer];
 
     dispatch_async(dispatch_get_main_queue(), ^{
-        NSFont *mono = [NSFont monospacedSystemFontOfSize:12.0 weight:NSFontWeightRegular];
+        NSFont *mono = [NSFont monospacedDigitSystemFontOfSize:12 weight:NSFontWeightRegular];
+
         statusItem.button.attributedTitle = [[NSAttributedString alloc]
-            initWithString:bt attributes:@{NSFontAttributeName: mono}];
-        if (vizMenuItem)   [vizMenuItem   setAttributedTitle:buildHourViz(h)];
-        if (secsMenuItem)  [secsMenuItem  setAttributedTitle:buildSecViz(h, s)];
-        if (pctItem)       [pctItem       setTitle:pct];
-        if (countdownItem) [countdownItem setTitle:cd];
-        if (targetItem)    [targetItem    setTitle:tl];
+            initWithString:t
+                attributes:@{NSFontAttributeName: mono}];
+
+        NSMutableAttributedString *full = [[NSMutableAttributedString alloc] init];
+
+        [full appendAttributedString:buildHeader()];
+        [full appendAttributedString:buildHourViz(h)];
+        [full appendAttributedString:buildMinuteViz(m)];
+        [full appendAttributedString:buildSecViz(s)];
+        [full appendAttributedString:buildLegend()];
+
+        [full appendAttributedString:[[NSAttributedString alloc]
+            initWithString:[@"\n\n" stringByAppendingString:f]
+                attributes:@{
+                    NSFontAttributeName: [NSFont systemFontOfSize:12],
+                    NSForegroundColorAttributeName: [NSColor whiteColor]
+                }]];
+
+        [mainItem setAttributedTitle:full];
     });
 }
 
 void runApp() { [[NSApplication sharedApplication] run]; }
 */
 import "C"
+
 import (
 	"fmt"
 	"math"
-	"os"
+	"runtime"
 	"time"
 	"unsafe"
 )
@@ -175,30 +242,16 @@ func cstr(s string) *C.char { return C.CString(s) }
 func cfree(p *C.char)       { C.free(unsafe.Pointer(p)) }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Usage: countdown <YYYY-MM-DD> [\"Label\"]")
-		os.Exit(1)
-	}
-	targetDate, err := time.ParseInLocation("2006-01-02", os.Args[1], time.Local)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Invalid date %q — use YYYY-MM-DD\n", os.Args[1])
-		os.Exit(1)
-	}
-	target := time.Date(
-		targetDate.Year(), targetDate.Month(), targetDate.Day(),
-		23, 59, 59, 0, time.Local,
-	)
-	label := "Target"
-	if len(os.Args) >= 3 {
-		label = os.Args[2]
-	}
+	runtime.LockOSThread()
+
+	targetDate, _ := time.Parse("2006-01-02", "2026-12-31")
+	target := time.Date(targetDate.Year(), targetDate.Month(), targetDate.Day(), 23, 59, 59, 0, time.Local)
 
 	C.initApp()
-	init := cstr("⏳ loading…")
+
+	init := cstr("⏳")
 	C.createStatusItem(init)
 	cfree(init)
-
-	targetStr := fmt.Sprintf("🎯  %s  —  %s", label, targetDate.Format("Jan 2, 2006"))
 
 	go func() {
 		for {
@@ -206,34 +259,28 @@ func main() {
 			H, M, S := now.Hour(), now.Minute(), now.Second()
 
 			diff := target.Sub(now)
-			var barTitle, cdStr string
-			if diff <= 0 {
-				barTitle = "🎉 " + label + " — Done!"
-				cdStr = "🏁  Target date has passed"
-			} else {
+
+			var d, h, m, s int
+			if diff > 0 {
 				total := int(math.Ceil(diff.Seconds()))
-				d := total / 86400
-				h := (total % 86400) / 3600
-				m := (total % 3600) / 60
-				s := total % 60
-				barTitle = fmt.Sprintf("⏳ %dd %02dh %02dm %02ds", d, h, m, s)
-				cdStr = fmt.Sprintf("⏱  %d days  %02dh %02dm %02ds remaining", d, h, m, s)
+				d = total / 86400
+				h = (total % 86400) / 3600
+				m = (total % 3600) / 60
+				s = total % 60
 			}
 
-			secsToday := H*3600 + M*60 + S
-			pct := secsToday * 100 / 86400
-			hoursLeft := 23 - H
-			pctStr := fmt.Sprintf("📅  Day used: %d%%  ·  %d hours left today", pct, hoursLeft)
+			pct := (H*3600 + M*60 + S) * 100 / 86400
 
-			bt := cstr(barTitle)
-			cd := cstr(cdStr)
-			tl := cstr(targetStr)
-			pl := cstr(pctStr)
-			C.updateAll(bt, C.int(H), C.int(S), pl, cd, tl)
-			cfree(bt)
-			cfree(cd)
-			cfree(tl)
-			cfree(pl)
+			title := fmt.Sprintf("⏳ %dd %02dh %02dm %02ds", d, h, m, s)
+			footer := fmt.Sprintf("⏳ %dd %02dh %02dm %02ds    TODAY USED %d%%", d, h, m, s, pct)
+
+			t := cstr(title)
+			f := cstr(footer)
+
+			C.updateUI(t, C.int(H), C.int(M), C.int(S), f)
+
+			cfree(t)
+			cfree(f)
 
 			time.Sleep(time.Second)
 		}
